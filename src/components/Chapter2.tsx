299dedb6de8d3e1878981f847abb31c62179bcca
DravishShah <dravishshah@gmail.com>
2026-07-06 15:24:23 +0530

    6-7-26

--- src/components/Chapter2.tsx
+++ src/components/Chapter2.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import Image from "next/image";
 import { useEffect, useRef } from "react";
 import * as THREE from "three";
 import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader.js";
